ae79a2b51c6689ec9d2a629284f20019556dd16e
stephbome <157913449+stephbome@users.noreply.github.com>
2025-07-01 11:59:28 +0100

    Add files via upload

---
+++ class 2/Class/src/ArrayExample.java
@@ -0,0 +1,25 @@
+packet Basics ;
+
+import java.util.Scanner;
+
+public class ArrayExample {
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+
+        int[] myArray = [1,3,5,7,9}
+        int[] anotherArray = new int[10];
+        String[] gender = {"Female","Male"};
+
+        for (int i = 0; i < anotherArray.length; i++){
+            System.out.println(myArray[i]);
+            anotherArray[i] = input.nextINT();
+    }
+    myArray[1] = 13;
+    myArray[2] = 13;
+    anotherArray[5] = 17;
+
+    for (int 1 = 0; i <anotherArray.length; i++){
+        System.out.println("Item at: " + 1 + "is" + anotherArray[i]);
+    }
+    }
+}
---
+++ class 2/Class/src/COSCLASS/User.java
@@ -0,0 +1,4 @@
+package COSCLASS;
+
+public class User {
+}
